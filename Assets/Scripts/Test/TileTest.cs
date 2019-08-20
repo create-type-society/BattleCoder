d769b1169f57c698db321870ed4e8f893dc91613
Kai <aria7716@gmail.com>
2019-08-21 00:35:37 +0900

    とりあえずtilemap取れるようにした

---
+++ Assets/Scripts/Test/TileTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileTest : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TileInfo tileInfo = new TileInfo(tilemap);
+        tileInfo.GetTile(2, 2);
+    }
+}
---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public struct Tiles
+{
+    public int Tanc;
+    public int Rock;
+    public int Hole;
+    public int Empty;
+}
+
+public class TileInfo : MonoBehaviour
+{
+    private Tilemap tilemap = new Tilemap();
+    private TileBase tiles;
+
+    public TileInfo(Tilemap tilemap)
+    {
+        this.tilemap = tilemap;
+    }
+
+    public void GetTile(int x, int y)
+    {
+        tiles = tilemap.GetTile(new Vector3Int(x, y, 0));
+        Debug.Log(tiles);
+    }
+}

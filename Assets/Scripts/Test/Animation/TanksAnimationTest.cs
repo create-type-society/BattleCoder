07dc1244bc1a35ad3a241b83518358787da52035
hiroki19990625 <hiroki19990625@gmail.com>
2019-08-21 00:21:53 +0900

    tanksのアニメーション完成

--- Assets/Scripts/Test/Animation/TanksAnimationTest.cs
+++ Assets/Scripts/Test/Animation/TanksAnimationTest.cs
@@ -8,11 +8,12 @@ namespace BattleCoder.Test.Animation
 
         private int tick = 0;
 
+        private bool toggle = false;
+
         private Animator _animator;
         private static readonly int X = Animator.StringToHash("X");
         private static readonly int Y = Animator.StringToHash("Y");
-        private static readonly int DirX = Animator.StringToHash("DirX");
-        private static readonly int DirY = Animator.StringToHash("DirY");
+        private static readonly int IsMove = Animator.StringToHash("IsMove");
 
         // Start is called before the first frame update
         void Start()
@@ -28,62 +29,32 @@ namespace BattleCoder.Test.Animation
                 case 0:
                     _animator.SetInteger(X, 0);
                     _animator.SetInteger(Y, -1);
-                    _animator.SetInteger(DirX, 0);
-                    _animator.SetInteger(DirY, 0);
                     break;
 
                 case 1:
                     _animator.SetInteger(X, 1);
                     _animator.SetInteger(Y, 0);
-                    _animator.SetInteger(DirX, 0);
-                    _animator.SetInteger(DirY, 0);
                     break;
 
                 case 2:
                     _animator.SetInteger(X, 0);
                     _animator.SetInteger(Y, 1);
-                    _animator.SetInteger(DirX, 0);
-                    _animator.SetInteger(DirY, 0);
                     break;
 
                 case 3:
                     _animator.SetInteger(X, -1);
                     _animator.SetInteger(Y, 0);
-                    _animator.SetInteger(DirX, 0);
-                    _animator.SetInteger(DirY, 0);
-                    break;
-
-                case 4:
-                    _animator.SetInteger(X, 0);
-                    _animator.SetInteger(Y, 0);
-                    _animator.SetInteger(DirX, 0);
-                    _animator.SetInteger(DirY, -1);
-                    break;
-
-                case 5:
-                    _animator.SetInteger(X, 0);
-                    _animator.SetInteger(Y, 0);
-                    _animator.SetInteger(DirX, 1);
-                    _animator.SetInteger(DirY, 0);
-                    break;
-
-                case 6:
-                    _animator.SetInteger(X, 0);
-                    _animator.SetInteger(Y, 0);
-                    _animator.SetInteger(DirX, 0);
-                    _animator.SetInteger(DirY, 1);
-                    break;
-
-                case 7:
-                    _animator.SetInteger(X, 0);
-                    _animator.SetInteger(Y, 0);
-                    _animator.SetInteger(DirX, -1);
-                    _animator.SetInteger(DirY, 0);
                     break;
             }
 
+            if (tick % 100 == 0)
+            {
+                _animator.SetBool(IsMove, toggle);
+                toggle = !toggle;
+            }
+
             if (tick++ % 200 == 0)
-                type = type == 7 ? 0 : ++type;
+                type = type == 3 ? 0 : ++type;
         }
     }
 }

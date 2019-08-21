0afd1b58b19b0926c3461d2c7cd9d96fd0be737e
elipmoc <relipmoc101@gmail.com>
2019-08-21 10:03:11 +0900

    コメント文追加

--- Assets/Scripts/GameObject/BotApplication/BotApplication.cs
+++ Assets/Scripts/GameObject/BotApplication/BotApplication.cs
@@ -1,4 +1,6 @@
-﻿public class BotApplication
+﻿//ボットを外部から簡単に制御できるようにするクラス
+
+public class BotApplication
 {
     readonly BotEntity botEntity;
     readonly CommandObjectController commandObjectController = new CommandObjectController();
@@ -8,12 +10,15 @@
         this.botEntity = botEntity;
     }
 
+    //移動コマンドの発行
     public void Move(Direction direction, uint speed, uint gridDistance)
     {
         var moveCommandObject = new MoveCommandObject(botEntity, direction, speed, gridDistance);
         commandObjectController.AddMoveTypeCommandObject(moveCommandObject);
     }
 
+    //色々な更新
+    //毎フレーム1回だけ呼んでください
     public void Update()
     {
         commandObjectController.RunCommandObjects();

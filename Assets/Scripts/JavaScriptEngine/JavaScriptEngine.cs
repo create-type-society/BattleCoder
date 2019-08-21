91cef7a2f839856611011d6a21489afb52b117fc
elipmoc <relipmoc101@gmail.com>
2019-08-21 11:03:34 +0900

    JSで移動制御実装

---
+++ Assets/Scripts/JavaScriptEngine/JavaScriptEngine.cs
@@ -0,0 +1,24 @@
+﻿//javaScriptのインタプリタエンジン
+
+using System;
+using Jint;
+using Jint.Runtime.Interop;
+
+public class JavaScriptEngine
+{
+    readonly IBotCommands botCommands;
+    readonly Engine engine;
+
+    public JavaScriptEngine(IBotCommands botCommands)
+    {
+        this.botCommands = botCommands;
+        engine = new Engine();
+        engine.SetValue("Direction", TypeReference.CreateTypeReference(engine, typeof(Direction)));
+        engine.SetValue("Move", new Action<Direction, float, uint>(botCommands.Move));
+    }
+
+    public void ExecuteJS(string script)
+    {
+        engine.Execute(script);
+    }
+}

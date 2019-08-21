062ada2ca995dac59e8bddc6b57f4f5f1bdb6b00
elipmoc <relipmoc101@gmail.com>
2019-08-21 09:50:30 +0900

    移動を実装

--- Assets/Scripts/GameObject/BotApplication/BotApplication.cs
+++ Assets/Scripts/GameObject/BotApplication/BotApplication.cs
@@ -1,3 +1,21 @@
 ﻿public class BotApplication
 {
+    readonly BotEntity botEntity;
+    readonly CommandObjectController commandObjectController = new CommandObjectController();
+
+    public BotApplication(BotEntity botEntity)
+    {
+        this.botEntity = botEntity;
+    }
+
+    public void Move(Direction direction, uint speed, uint gridDistance)
+    {
+        var moveCommandObject = new MoveCommandObject(botEntity, direction, speed, gridDistance);
+        commandObjectController.AddMoveTypeCommandObject(moveCommandObject);
+    }
+
+    public void Update()
+    {
+        commandObjectController.RunCommandObjects();
+    }
 }

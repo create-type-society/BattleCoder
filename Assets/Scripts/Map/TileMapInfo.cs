4bcefb1b93d2b5fa1334f95b5725d59e42271384
Kai <aria7716@gmail.com>
2019-08-21 12:28:17 +0900

    tileの種別取得できるようにした

--- Assets/Scripts/Map/TileMapInfo.cs
+++ Assets/Scripts/Map/TileMapInfo.cs
@@ -1,30 +1,26 @@
-﻿using BattleCoder.Map;
+﻿using System.Text.RegularExpressions;
+using BattleCoder.Map;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 public class TileMapInfo : MonoBehaviour
 {
-    [SerializeField] private Tilemap tilemap;
+    private Tilemap tilemap;
     private TileBase tile;
     private GridPosition pos;
     
-    public TileType GetTileType(GridPosition pos)
+    public TileType GetTileType(GridPosition pos,Tilemap tilemap)
     {
+        this.tilemap = tilemap;
         tile = tilemap.GetTile(new Vector3Int(pos.X,pos.Y, 0));
         return SearchTileType(tile);
     }
 
     private TileType SearchTileType(TileBase tile)
     {
-        switch (tile.name)
-        {
-            case @"[1-9]*_tanc":
-                return TileType.tanc;
-            case @"[1-9]*_rock":
-                return TileType.rock;
-            case @"[1-9]*_hole":
-                return TileType.hole;
-            default:
-                return TileType.empty;
-        }
+        if (tile == null) return TileType.empty;
+        if (tile.name.Contains("tanc")) return TileType.tanc;
+        if (tile.name.Contains("rock")) return TileType.rock;
+        if (tile.name.Contains("hole")) return TileType.hole;
+        return TileType.empty;
     }
 }
--- Assets/Scripts/Test/TileTest.cs
+++ Assets/Scripts/Test/TileTest.cs
@@ -1,21 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using BattleCoder.Map;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine.Tilemaps;
 
 public class TileTest : MonoBehaviour
 {
-    public Tilemap tilemap;
+    [SerializeField] private Tilemap tilemap;
 
+    [SerializeField] private GridPosition pos = new GridPosition(0,0);
+
+    private TileMapInfo tileInfo;
     // Start is called before the first frame update
     void Start()
     {
+        
+        tileInfo = gameObject.AddComponent<TileMapInfo>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        TileInfo tileInfo = new TileInfo(tilemap);
-        tileInfo.GetTile(0, 0);
+        Debug.Log(tileInfo.GetTileType(pos,tilemap));
     }
 }

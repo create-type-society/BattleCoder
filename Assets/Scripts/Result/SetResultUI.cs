6e27e46d7b5a000698433213a295000606112afb
Kai <aria7716@gmail.com>
2019-08-22 15:28:18 +0900

    Result画面の作成

---
+++ Assets/Scripts/Result/SetResultUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SetResultUI : MonoBehaviour
+{
+    [SerializeField] private Text resuntText;
+    
+    void Start()
+    { 
+        CheckResult();
+    }
+
+    private void CheckResult()
+    {
+        string text;
+        Color color;
+        ResultInfo.SetResult(true);
+        if (ResultInfo.GetResult())
+        {
+            color = Color.red;
+            text = "YOU WIN";
+        }
+        else
+        {   
+            
+            color = new Color(0,65,255);
+            text = "YOU LOSE";
+        }
+        SetText(text,color);
+    }
+
+    private void SetText(string text, Color color)
+    {
+        resuntText.color = color;
+        resuntText.text = text;
+    }
+    
+}

1ed7908b0afe5789dcd714909cd1e654392072d1
Kai <aria7716@gmail.com>
2019-08-24 14:22:18 +0900

    SoundManager作成

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]AudioSource firingSound;
+    [SerializeField]AudioSource meleeSound;
+    
+    public void MakeFiringSound()
+    {
+        firingSound.Play();
+    }
+    public void MakeMeleeSound()
+    {
+        meleeSound.Play();
+    }
+    
+    
+}

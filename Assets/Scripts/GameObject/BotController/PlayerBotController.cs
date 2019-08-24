7490f2314f5a5c8724a8b74d4e4bdf36592a262c
elipmoc <relipmoc101@gmail.com>
2019-08-24 14:32:29 +0900

    Godの処理を分離

---
+++ Assets/Scripts/GameObject/BotController/PlayerBotController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BattleCoder.GamePlayUi;
+using UnityEngine.Experimental.PlayerLoop;
+using Object = UnityEngine.Object;
+
+public class PlayerBotController
+{
+    readonly IUserInput userInput = new KeyController();
+    readonly BotApplication botApplication;
+    readonly PlayerHpPresenter playerHpPresenter;
+
+    public PlayerBotController(BotEntity botEntityPrefab, TileMapInfo tileMapInfo, BulletEntity bulletPrefab,
+        CameraFollower cameraFollower, PlayerHpPresenter playerHpPresenter, RunButtonEvent runButtonEvent,
+        ScriptText scriptText, ErrorMsg errorMsg)
+    {
+        this.playerHpPresenter = playerHpPresenter;
+        var botEntity = Object.Instantiate(botEntityPrefab);
+        cameraFollower.SetPlayerPosition(botEntity.transform);
+        var botEntityAnimation = botEntity.GetComponent<BotEntityAnimation>();
+        botApplication = new BotApplication(botEntity, botEntityAnimation, tileMapInfo, bulletPrefab);
+        userInput.ShootingAttackEvent += (sender, e) => { botApplication.Shot(); };
+
+        var javaScriptEngine = new JavaScriptEngine(botApplication);
+        runButtonEvent.AddClickEvent(() =>
+        {
+            try
+            {
+                javaScriptEngine.ExecuteJS(scriptText.GetScriptText());
+            }
+            catch (Exception e)
+            {
+                errorMsg.SetText(e.ToString());
+            }
+        });
+    }
+
+    public void Update()
+    {
+        botApplication.Update();
+        userInput.Update();
+        playerHpPresenter.RenderHp(botApplication.Hp);
+    }
+
+    public bool IsDeath()
+    {
+        return botApplication.Hp.IsDeath();
+    }
+}

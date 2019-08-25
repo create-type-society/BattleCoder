62d28bfa4668a4f94e6f7915bea48bca709507b0
Bester8723 <lunamimi0315777@yahoo.co.jp>
2019-08-25 15:38:49 +0900

    title
    タイトル画面追加

---
+++ Assets/Scripts/Title/Scene移動.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene移動 : MonoBehaviour
+{
+
+    // ボタンをクリックするとBattleSceneに移動します
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("SinglePlayGameScene");
+    }
+}

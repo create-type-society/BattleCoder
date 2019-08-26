c4675562426027399694177418df0425b5600bd0
Bester8723 <lunamimi0315777@yahoo.co.jp>
2019-08-26 17:42:57 +0900

    タイトル画面変更です

--- Assets/Scripts/Title/SceneChanger.cs
+++ Assets/Scripts/Title/SceneChanger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Scene移動 : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
 
     // ボタンをクリックするとBattleSceneに移動します

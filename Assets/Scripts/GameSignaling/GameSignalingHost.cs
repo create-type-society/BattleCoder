07e6c95cf43a4ae92bbf365f12fe46424ca64d5f
elipmoc <relipmoc101@gmail.com>
2019-08-29 04:32:09 +0900

    Gameプレイ中のデータをやり取りする基本のシステムだけ実装

---
+++ Assets/Scripts/GameSignaling/GameSignalingHost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+public class GameSignalingHost
+{
+    readonly MyTcpClient myTcpClient;
+    public event Action<HostReceiveSignalData> ReceivedHostReceiveSignalData;
+
+    public GameSignalingHost(MyTcpClient myTcpClient)
+    {
+        this.myTcpClient = myTcpClient;
+    }
+
+    public void SendData(ClientReceiveSignalData data)
+    {
+        myTcpClient.WriteData(JsonConvert.SerializeObject(data));
+    }
+
+    public void Update()
+    {
+        var result = myTcpClient.ReadData();
+        if (result.isOk)
+            ReceivedHostReceiveSignalData?.Invoke(
+                JsonConvert.DeserializeObject<HostReceiveSignalData>(result.data)
+            );
+    }
+}

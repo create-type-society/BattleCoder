1d574dfe6f69bd2c40b68f5ad5f1745bf4b5f675
Kai <aria7716@gmail.com>
2019-08-28 12:22:51 +0900

    tank同士が同じ位置に行けないようにした

--- Assets/Scripts/GameObject/BotApplication/Bot/BotEntity.cs
+++ Assets/Scripts/GameObject/BotApplication/Bot/BotEntity.cs
@@ -7,13 +7,13 @@ public class BotEntity : MonoBehaviour
     // 弾当たった時ののイベント
     public event EventHandler<EventArgs> HitBulletEvent;
 
-    //横に移動する(岩があったら停止してfalse返す)
+    //横に移動する(岩か敵があったら停止してfalse返す)
     public bool MoveX(float x, TileMapInfo tileMapInfo)
     {
         return Move(x, 0, tileMapInfo);
     }
 
-    //縦に移動する(岩があったら停止してfalse返す)
+    //縦に移動する(岩か敵があったら停止してfalse返す)
     public bool MoveY(float y, TileMapInfo tileMapInfo)
     {
         return Move(0, y, tileMapInfo);
@@ -21,7 +21,7 @@ public class BotEntity : MonoBehaviour
 
     bool Move(float x, float y, TileMapInfo tileMapInfo)
     {
-        if (RockCheck(x, y, tileMapInfo)) return false;
+        if (RockCheck(x, y, tileMapInfo) || TancCheck(x,y,tileMapInfo)) return false;
         transform.position += new Vector3(x, y, 0);
         return true;
     }
@@ -33,6 +33,22 @@ public class BotEntity : MonoBehaviour
         y = y > 0 ? y + gridSizeHalf : y < 0 ? y - gridSizeHalf : y;
         var tileType = tileMapInfo.GetTileType(transform.position + new Vector3(x, y, 0));
         return tileType == TileType.rock;
+    }
+    
+    bool TancCheck(float x, float y, TileMapInfo tileMapInfo)
+    {
+        var gridSizeHalf = Global.GridSize / 2;
+        x = x > 0 ? x + gridSizeHalf : x < 0 ? x - gridSizeHalf : x;
+        y = y > 0 ? y + gridSizeHalf : y < 0 ? y - gridSizeHalf : y;
+        GridPosition movePos = tileMapInfo.GetGridPosition(transform.position + new Vector3(x, y, 0));
+        var tileType = tileMapInfo.GetTileType(transform.position + new Vector3(x, y, 0));
+        Debug.Log(tileType);
+        if (tileType == TileType.tank)
+        {
+            GridPosition myPos = tileMapInfo.GetGridPosition(transform.position);
+            return !(movePos.X == myPos.X && movePos.Y == myPos.Y);
+        } 
+        return false;
     }
 
     void OnTriggerEnter2D(Collider2D other)

e6b0f754dd76827c12434719a5757fe2c8582041
elipmoc <relipmoc101@gmail.com>
2019-08-28 13:10:51 +0900

    マッチングサーバーマッチするだけ完成

---
+++ Assets/Scripts/Matching/MatchingServer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+public class MatchingServer
+{
+    MyTcpClient myTcpClient;
+
+    //マッチ成功した時に呼ばれる時に呼ばれる処理
+    public event Action Matched;
+
+    public MatchingServer(MyTcpClient myTcpClient)
+    {
+        this.myTcpClient = myTcpClient;
+    }
+
+    public void Update()
+    {
+        while (true)
+        {
+            var result = myTcpClient.ReadData();
+            if (result.isOk == false) return;
+            if (result.data == "match") Matched?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Test/Tcp/TcpTestComponent.cs
+++ Assets/Scripts/Test/Tcp/TcpTestComponent.cs
@@ -5,23 +5,25 @@ namespace BattleCoder.Test.Tcp
     public class TcpTestComponent : MonoBehaviour
     {
         readonly MyTcpClient client = new MyTcpClient("localhost", 3000);
+
+        MatchingServer matchingServer;
         int count = 0;
 
         void Start()
         {
             client.Connect();
             client.DisConnected += () => Debug.Log("TCP切断");
+            matchingServer = new MatchingServer(client);
+            matchingServer.Matched += () => { Debug.Log("マッチ"); };
         }
 
         void Update()
         {
+            matchingServer.Update();
             count++;
             if (count % 60 == 0)
                 client.WriteData("ほげ");
-            var result = client.ReadData();
-            if (result.isOk)
-                Debug.Log(result.data);
-            if(count>600)
+            if (count > 600)
                 client.DisConnect();
         }
     }

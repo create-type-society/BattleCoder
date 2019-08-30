a487528c9542624d9784db99c586552b8a266048
Bester8723 <lunamimi0315777@yahoo.co.jp>
2019-08-30 19:10:11 +0900

    近接攻撃を追加

---
+++ Assets/Scripts/GameObject/BotApplication/MeleeAttackApplication/MeleeAttackApplication.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackApplication
+{
+    MeleeAttackEntity meleeAttackEntity;
+
+    public MeleeAttackApplication(MeleeAttackEntity meleeAttackEntity)
+    {
+        this.meleeAttackEntity = meleeAttackEntity;
+    }
+    public void MeleeAttack(Vector3 position, Direction direction)
+    {
+        meleeAttackEntity.MeleeAttack(position, direction);
+    }
+}

5dbf0c92d8de3f67b2c4de98bc4cb7d6961caac5
elipmoc <relipmoc101@gmail.com>
2019-09-02 04:23:00 +0900

    プロセススクロールビュー追加

---
+++ Assets/Scripts/GamePlayUi/ProcessPanelPresenter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProcessPanelPresenter : MonoBehaviour
+{
+    [SerializeField] Button closeButton;
+
+    public event Action closed;
+
+    void Awake()
+    {
+        closeButton.onClick.AddListener(() => closed?.Invoke());
+    }
+}
---
+++ Assets/Scripts/GamePlayUi/ProcessScrollViewPresenter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ProcessScrollViewPresenter : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+
+    [SerializeField] ProcessPanelPresenter processPanelPrefab;
+
+    void Start()
+    {
+        for (var i = 0; i < 10; i++)
+            Instantiate(processPanelPrefab, content.transform);
+    }
+}

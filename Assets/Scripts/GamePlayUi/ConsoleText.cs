4b5cfeec96203ef1a25893ac124e4a310ad5b13d
elipmoc <relipmoc101@gmail.com>
2019-09-02 22:00:57 +0900

    コンソールからの表示文字列を200行以内になるように

--- Assets/Scripts/GamePlayUi/ConsoleText.cs
+++ Assets/Scripts/GamePlayUi/ConsoleText.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,11 +8,9 @@ public class ConsoleText : MonoBehaviour
     [SerializeField] private Text consoleText;
     [SerializeField] private ScrollRect scrollRect;
 
-    public void AppendText(string text)
-    {
-        consoleText.text += text;
-        scrollRect.verticalNormalizedPosition = 0;
-    }
+    int lineCount = 0;
+    const int maxLineCount = 200;
+
 
     public void AppendTextNewLine(string text)
     {
@@ -21,5 +20,29 @@ public class ConsoleText : MonoBehaviour
     public void ClearText()
     {
         consoleText.text = "";
+    }
+
+    private void AppendText(string text)
+    {
+        consoleText.text += text;
+        lineCount++;
+        if (lineCount > maxLineCount)
+        {
+            lineCount = maxLineCount/2;
+            consoleText.text = CutText(consoleText.text);
+        }
+
+        scrollRect.verticalNormalizedPosition = 0;
+    }
+
+    private string CutText(string text)
+    {
+        return
+            string.Join(
+                Environment.NewLine,
+                text
+                    .Split(new string[] {Environment.NewLine}, StringSplitOptions.None)
+                    .Skip(maxLineCount / 2)
+            );
     }
 }

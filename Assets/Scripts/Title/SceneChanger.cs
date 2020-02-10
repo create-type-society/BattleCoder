f3b5532f8badd80046578975fecd550ce556608f
elipmoc <relipmoc101@gmail.com>
2020-02-11 01:23:50 +0900

    タイトルのボタンを押したら、決定効果音が鳴るように

--- Assets/Scripts/Title/SceneChanger.cs
+++ Assets/Scripts/Title/SceneChanger.cs
@@ -1,17 +1,27 @@
-﻿using UnityEngine;
+﻿using BattleCoder.Sound;
+using UnityEngine;
 
 namespace BattleCoder.Title
 {
     public class SceneChanger : MonoBehaviour
     {
+        [SerializeField] SoundManager soundManager;
+        [SerializeField] AudioSource bgm;
+
         // ボタンをクリックするとBattleSceneに移動します
-        public void SingleButtonClicked()
+        public async void SingleButtonClicked()
         {
+            bgm.Stop();
+            soundManager.MakeDecisionSound();
+            await UniRx.Async.UniTask.Delay(500);
             SceneChangeManager.ChangeSinglePlayStageSelect();
         }
 
-        public void MultiButtonClicked()
+        public async void MultiButtonClicked()
         {
+            bgm.Stop();
+            soundManager.MakeDecisionSound();
+            await UniRx.Async.UniTask.Delay(500);
             SceneChangeManager.ChangeMatchingScene();
         }
     }

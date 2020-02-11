9f239093c8d8c78076898ba8349d0cdf68ee5f5b
elipmoc <relipmoc101@gmail.com>
2020-02-12 01:03:16 +0900

    CommandPalletの制御するクラス作成

---
+++ Assets/Scripts/GamePlayUi/CommandPallet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandPallet : MonoBehaviour
+{
+    int cnt = 0;
+    [SerializeField] InputField inputField;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cnt++;
+        if (cnt % 80 == 0)
+        {
+            var text = inputField.text;
+            inputField.text = text.Insert(inputField.caretPosition, UnityEngine.Random.value + "");
+        }
+    }
+}

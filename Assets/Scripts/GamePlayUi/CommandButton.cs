f731ffcdd264435dcee6357cb62417ef5fda1204
elipmoc <relipmoc101@gmail.com>
2020-02-12 14:41:53 +0900

    コマンドパレットのボタンからプログラムを挿入できるように

---
+++ Assets/Scripts/GamePlayUi/CommandButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BattleCoder.GamePlayUi
+{
+    public class CommandButton : MonoBehaviour
+    {
+        Action<string> callback;
+
+        [SerializeField] string text;
+
+        void Start()
+        {
+            GetComponent<Button>()
+                .OnClickAsObservable().Subscribe((_) => OnClick());
+        }
+
+        public void SetClickedCallback(Action<string> f)
+        {
+            callback = f;
+        }
+
+        public void OnClick()
+        {
+            callback(text);
+        }
+    }
+}
--- Assets/Scripts/GamePlayUi/CommandPallet.cs
+++ Assets/Scripts/GamePlayUi/CommandPallet.cs
@@ -1,23 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using UniRx.Triggers;
+using UniRx;
+using UniRx.Async;
+using UnityEngine;
 using UnityEngine.UI;
 
-public class CommandPallet : MonoBehaviour
+namespace BattleCoder.GamePlayUi
 {
-    int cnt = 0;
-    [SerializeField] InputField inputField;
-
-    void Start()
+    public class CommandPallet : MonoBehaviour
     {
-    }
+        [SerializeField] InputField inputField;
+        int caretPosition = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-        cnt++;
-        if (cnt % 80 == 0)
+        void Start()
+        {
+            foreach (var x in GetComponentsInChildren<CommandButton>())
+            {
+                x.SetClickedCallback(AddText);
+            }
+        }
+
+        void Update()
+        {
+            if (inputField.isFocused)
+                caretPosition = inputField.caretPosition;
+        }
+
+        async void AddText(string newText)
         {
             var text = inputField.text;
-            inputField.text = text.Insert(inputField.caretPosition, UnityEngine.Random.value + "");
+            inputField.text = text.Insert(caretPosition, newText);
+            var tmpCaretPosition = caretPosition + newText.Length;
+            inputField.Select();
+            await UniTask.DelayFrame(0);
+            inputField.selectionFocusPosition = tmpCaretPosition;
+            inputField.selectionAnchorPosition = tmpCaretPosition;
+            inputField.caretPosition = tmpCaretPosition;
         }
     }
 }
